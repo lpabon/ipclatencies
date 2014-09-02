e7458bf19d60aeeacc8663c2408d9358e7149338
Luis Pabón <lpabon@redhat.com>
2014-09-02 01:42:54 -0400

    Listen on multiple zeromq sockets

--- hw3client.go
+++ hw3client.go
@@ -8,36 +8,74 @@ package main
 
 import (
 	"fmt"
+	"github.com/lpabon/bufferio"
 	"github.com/lpabon/foocsim/utils"
+	"github.com/lpabon/godbc"
 	zmq "github.com/pebbe/zmq3"
+	"sync"
 	"time"
 )
 
-func main() {
-	context, _ := zmq.NewContext()
+type Message struct {
+	Id     int32
+	Count  int32
+	Result int32
+}
+
+func client(wg sync.WaitGroup, context *zmq.Context, id int) {
+	defer wg.Done()
 	socket, _ := context.NewSocket(zmq.REQ)
 	// defer context.Close()
 	defer socket.Close()
 
-	fmt.Printf("Connecting to latency server...\n")
+	fmt.Printf("%d: Connecting to latency server...\n", id)
 	socket.Connect("ipc://latency.ipc")
 	//socket.Connect("tcp://*:5555")
 
-	msg := []byte{0}
+	var msg = Message{Id: int32(id)}
+	buf := bufferio.NewBufferIOMake(32)
+
 	var td utils.TimeDuration
 	prev := td.Copy()
 
-	for i := 0; i < 1000000; i++ {
+	for i := 1; i < 1000000; i++ {
+		buf.Reset()
+		msg.Count = int32(i)
+		err := buf.WriteDataLE(msg)
+		godbc.Check(err == nil, fmt.Sprintln(err))
+
+		time.Sleep(time.Microsecond * 50)
+
 		start := time.Now()
-		socket.SendBytes([]byte(msg), 0)
-		socket.RecvBytes(0)
+		socket.SendBytes(buf.Bytes(), 0)
+		ret, _ := socket.RecvBytes(0)
 		end := time.Now()
+
+		var retmsg Message
+		retbuf := bufferio.NewBufferIO(ret)
+		err = retbuf.ReadDataLE(&retmsg)
+		godbc.Check(err == nil)
+		godbc.Check(retmsg.Id == int32(id))
+		godbc.Check(retmsg.Result == (msg.Count + 10))
+
 		td.Add(end.Sub(start))
 		if (i % 10000) == 0 {
-			fmt.Printf("%d: %.4f usecs\n", i, td.DeltaMeanTimeUsecs(prev))
+			fmt.Printf("%d:%d: %.4f usecs\n", id, i, td.DeltaMeanTimeUsecs(prev))
 			prev = td.Copy()
 		}
 	}
 
-	fmt.Printf("\nLatency %.4f usecs\n", td.MeanTimeUsecs())
+	fmt.Printf("\n%d:Latency %.4f usecs\n", id, td.MeanTimeUsecs())
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+	context, _ := zmq.NewContext()
+	for i := 0; i < 25; i++ {
+		wg.Add(1)
+		go client(wg, context, i)
+	}
+
+	wg.Wait()
 }
--- hw3server.go
+++ hw3server.go
@@ -7,11 +7,20 @@
 package main
 
 import (
+	"github.com/lpabon/bufferio"
+	"github.com/lpabon/godbc"
 	zmq "github.com/pebbe/zmq3"
+	"runtime"
+	"sync"
 )
 
-func main() {
-	context, _ := zmq.NewContext()
+type Message struct {
+	Id     int32
+	Count  int32
+	Result int32
+}
+
+func server(wg sync.WaitGroup, context *zmq.Context) {
 	socket, _ := context.NewSocket(zmq.REP)
 	//defer context.Close()
 	defer socket.Close()
@@ -21,6 +30,29 @@ func main() {
 	// Wait for messages
 	for {
 		msg, _ := socket.RecvBytes(0)
+
+		var retmsg Message
+		retbuf := bufferio.NewBufferIO(msg)
+		err := retbuf.ReadDataLE(&retmsg)
+		godbc.Check(err == nil)
+
+		retmsg.Result = retmsg.Count + 10
+		retbuf.Reset()
+		retbuf.WriteDataLE(retmsg)
+
 		socket.SendBytes(msg, 0)
 	}
+}
+
+func main() {
+
+	runtime.GOMAXPROCS(4)
+	var wg sync.WaitGroup
+	context, _ := zmq.NewContext()
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go server(wg, context)
+	}
+
+	wg.Wait()
 }
