0b6a16b190747b6022b7123cb425f4ca91c4d151
Luis Pabón <lpabon@redhat.com>
2015-05-24 02:57:08 -0400

    fixed c channels

--- unix_server_mt.c
+++ unix_server_mt.c
@@ -10,10 +10,14 @@
 
 char *socket_path = "go.sock";
 
-#define T() do {\
-    printf("%s:%d:%s()\n", __FILE__, __LINE__, __func__);\
+#ifdef LUIS
+#define T(fmt, ...) do {\
+    printf("%s:%d:%s(): " fmt "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__);\
     fflush(stdout);\
 }while(0)
+#else
+#define T(fmt, ...) do { }while(0)
+#endif
 
 typedef struct {
     int socket;
@@ -21,9 +25,10 @@ typedef struct {
     int len;
 } msg_t;
 
+
 typedef struct {
-    bool empty;
-    pthread_cond_t c;
+    pthread_cond_t send_c;
+    pthread_cond_t recv_c;
     pthread_mutex_t m;
     msg_t *msg;
 } channel_t;
@@ -41,23 +46,23 @@ typedef struct {
 
 void
 ch_init(channel_t *ch) {
-    ch->empty = true;
+    ch->msg = NULL;
     pthread_mutex_init(&ch->m, NULL);
-    pthread_cond_init(&ch->c, NULL);
+    pthread_cond_init(&ch->send_c, NULL);
+    pthread_cond_init(&ch->recv_c, NULL);
 }
 
 void
 ch_send(channel_t *ch, msg_t *msg) {
 
     pthread_mutex_lock(&ch->m);
-    while (!ch->empty) {
-        pthread_cond_wait(&ch->c, &ch->m);
+    while (NULL != ch->msg) {
+        pthread_cond_wait(&ch->send_c, &ch->m);
     }
 
-    ch->empty = false;
     ch->msg = msg;
 
-    pthread_cond_signal(&ch->c);
+    pthread_cond_signal(&ch->recv_c);
     pthread_mutex_unlock(&ch->m);
 }
 
@@ -67,14 +72,14 @@ ch_recv(channel_t *ch) {
     msg_t *msg;
 
     pthread_mutex_lock(&ch->m);
-    while (ch->empty) {
-        pthread_cond_wait(&ch->c, &ch->m);
+    while (NULL == ch->msg) {
+        pthread_cond_wait(&ch->recv_c, &ch->m);
     }
 
-    ch->empty = true;
     msg = ch->msg;
+    ch->msg = NULL;
 
-    pthread_cond_signal(&ch->c);
+    pthread_cond_signal(&ch->send_c);
     pthread_mutex_unlock(&ch->m);
 
     return msg;
@@ -88,12 +93,15 @@ worker(void *arg) {
     while (1) {
         /* get */
         msg = ch_recv(&conn->workers); 
+        T("s:%d got message", msg->socket);
 
         /* Work */
         usleep(1);
 
         /* Send */
+        T("s:%d @@send", msg->socket);
         ch_send(&conn->reply, msg);
+        T("s:%d sent++", msg->socket);
     }
 
     return NULL;
@@ -107,6 +115,7 @@ writer(void *arg) {
 
     while (1) {
         msg = ch_recv(&conn->reply);
+        T("msg ready to be sent");
 
         len = write(msg->socket, msg->buf, msg->len);
         if (len < 0) {
@@ -137,6 +146,7 @@ reader(void *arg) {
     s = (server_t *)arg;
     conn = s->conn;
 
+    T("start");
     while(1) {
         msg = (msg_t *)malloc(sizeof(msg_t));
 
@@ -153,8 +163,10 @@ reader(void *arg) {
             goto out;
         }
 
+        T("s:%d send", s->socket);
         /* send */
         ch_send(&conn->workers, msg); 
+        T("s:%d back", s->socket);
     }
 
 out:
@@ -178,7 +190,6 @@ int main(int argc, char *argv[]) {
     ch_init(&conn.reply);
     /* spawn */
     pthread_create(&tid, NULL, writer, &conn);
-
 
     // spawn workers
     for (i=0; i<32; i++) {
